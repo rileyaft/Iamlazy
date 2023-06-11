c9d6c2f495cef8ea13c3e31e15e93138133ffca8
rileyaft <115379744+rileyaft@users.noreply.github.com>
2023-06-11 12:11:20 -0700

    major update 1.1.0
    nice new checks and further use, plus no longer required API key

--- Iamlazy.sh
+++ Iamlazy.sh
@@ -12,7 +12,6 @@
 #  \____/ \__,_|_| |_| |_|_|\__,_/___|\__, |
 #                                     |___/
 
-
 #####################################
 ############# IMPORTANT #############
 #####################################
@@ -21,26 +20,68 @@
 
 
 # User Variables
-YOUR_TOKEN="your_token_here" # IMPORTANT: your Github API access key, make one at "https://github.com/settings/tokens"
-REPO="repo" # repo to grab from
-OWNER="owner" # owner of repo
-DIRECTORY="./Downloads" # desired file output (WILL REPLACE EXISTING FILE IF PRESENT)
-FILE="extension" # desired application file extension grabbed from releases, ie "7z"
-NAME="name" # application will be named this once complete
+YOUR_TOKEN="no_token" # Github API key for those who want to do large numbers of requests. Leave blank/untouched unless you need to download 60 files/min
+REPO="Iamlazy" # repo to grab from
+OWNER="rileyaft" # owner of repo
+DIRECTORY="$HOME/Iamlazy" # desired file output (WILL REPLACE EXISTING FILE IF PRESENT)
+FILE="tar.gz" # desired application file extension grabbed from releases, ie "7z"
+NAME="Iamlazy" # application will be named this once complete
 
+
+# Check if $DIRECTORY is valid
+check_directory() {
+  if [ ! -d "$DIRECTORY" ]; then
+    echo "Directory $DIRECTORY is invalid."
+    return 1
+  fi
+  return 0
+}
+
+# Loop to revalidate and prompt for new directory if needed - make the process less painful
+while ! check_directory; do
+  echo "Would you like to create new directory $DIRECTORY?"
+  echo "1. Yes"
+  echo "2. Insert new directory"
+  echo "3. Cancel"
+
+  read -p "Please enter number 1-3: " choice
+  case $choice in
+    1)
+      echo "Creating new directory $DIRECTORY"
+      mkdir -p "$DIRECTORY"
+      ;;
+    2)
+      read -p "Enter new directory: " DIRECTORY
+      ;;
+    3)
+      echo "Exiting script"
+      exit 1
+      ;;
+    *)
+      echo "Invalid option. Please try again"
+  esac
+done
+echo "Directory is valid"
 
 # Finding downloads
 echo "Reading $REPO ..."
-RESPONSE=$(curl -L \
+if [ "$YOUR_TOKEN" == "no_token" -o "$YOUR_TOKEN" == "" ]; then
+ grablatest=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$OWNER/$REPO/releases/latest)
+else
+ grablatest=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $YOUR_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$OWNER/$REPO/releases/latest)
+fi
 
-browser_download_url=$(echo "$RESPONSE" | grep -o "\"browser_download_url\": \"[^\"]**.$FILE\"" | sed 's/"browser_download_url": "//' | sed 's/"$//')
+browser_download_url=$(echo "$grablatest" | grep -o "\"browser_download_url\": \"[^\"]**.$FILE\"" | sed 's/"browser_download_url": "//' | sed 's/"$//')
 
 if [ -z "$browser_download_url" ]; then
-  echo "Error: No download URL found for *.$FILE in the latest release of $REPO. Check your configs."
+  echo "Error: No download URL found for any *.$FILE file in the latest release of $REPO. Check your configs."
   exit 1
 fi
 
@@ -66,7 +107,6 @@ script_loc=$(pwd) # check working directory
 
 # Moving the file to the desired directory
 echo "Moving the downloaded file to $DIRECTORY ..."
-cd /
 mv -f "$script_loc/$downloaded_file" "$DIRECTORY/"
 
 # Check if move was successful
@@ -80,16 +120,22 @@ fi
 
 # Renaming the file to the desired name and extension
 new_file="$DIRECTORY/$NAME.$FILE"
-echo "Renaming the file to $new_file ..."
-mv -f "$DIRECTORY/$downloaded_file" "$new_file"
 
-# Check if rename was successful
-rename_exit_code=$?
-if [ $rename_exit_code -eq 0 ]; then
-  echo "File $downloaded_file has been renamed to $new_file"
+if [ "$DIRECTORY/$downloaded_file" != "$new_file" ]; then
+  echo "Renaming the file to $new_file ..."
+  mv -f "$DIRECTORY/$downloaded_file" "$new_file"
+
+  rename_exit_code=$?
+  if [ $rename_exit_code -eq 0 ]; then
+    echo "File renamed successfully"
+  else
+    echo "Error: Failed to rename the file $downloaded_file to $new_file. Check the file permissions."
+    exit $rename_exit_code
+  fi
 else
-  echo "Error: Failed to rename the file $downloaded_file to $new_file. Check the file permissions."
-  exit $rename_exit_code
+  echo "Skipping file renaming as the source and destination paths are the same"
 fi
+
+
 echo "File $NAME.$FILE has been successfully downloaded and placed in target directory. Enjoy!"
 exit 0
